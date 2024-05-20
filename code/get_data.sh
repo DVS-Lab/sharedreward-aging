5f44c16e4bbab8138a9575bf077099cee8a3d4a9
David V. Smith <david.v.smith@temple.edu>
2024-05-20 10:10:05 -0400

    download notes and steps; partial

--- code/get_data.sh
+++ code/get_data.sh
@@ -4,4 +4,12 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
+# go to root and install datasets
+cd $maindir
+datalad install https://github.com/OpenNeuroDatasets/ds005123.git
+datalad install https://github.com/OpenNeuroDatasets/ds003745.git # not working; issue posted
+
+# get anatomical data and fmaps
+
+# get sharedreward data
 
