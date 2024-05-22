75d0d7681bcf7b71fd34b172b08c57aea0acfd9b
David V. Smith <david.v.smith@temple.edu>
2024-05-22 13:07:19 -0400

    run on srndna subs

--- code/run_fmriprep.sh
+++ code/run_fmriprep.sh
@@ -3,10 +3,10 @@
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-for sub in 104 10317; do
+for sub in `cat $scriptdir/sublist-srndna.txt`; do
 
 	script=${scriptdir}/fmriprep.sh
-	NCORES=3
+	NCORES=10
 	while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
 		sleep 5s
 	done
