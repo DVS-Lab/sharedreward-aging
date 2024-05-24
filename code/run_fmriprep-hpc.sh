b93d8f0fd7d319b0f2a9bff0158674155cee7e2e
cooper-sharp <tun31934@temple.edu>
2024-05-24 11:46:28 -0400

    Create script to run sharedreward preproc on cluster -- edit run script for use

---
+++ code/run_fmriprep-hpc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ensure paths are correct
+maindir=~/work/rf1-sra-data #this should be the only line that has to change if the rest of the script is set up correctly
+scriptdir=$maindir/code
+
+
+mapfile -t myArray < ${scriptdir}/sublist-rf1.txt
+
+
+# grab the first 10 elements
+ntasks=2
+counter=0
+while [ $counter -lt ${#myArray[@]} ]; do
+	subjects=${myArray[@]:$counter:$ntasks}
+	echo $subjects
+	let counter=$counter+$ntasks
+	qsub -v subjects="${subjects[@]}" sr-fmriprep-hpc.sh
+done
