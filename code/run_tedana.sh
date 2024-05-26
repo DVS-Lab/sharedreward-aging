5d046b0a8db80ab8707c14d6611bcf7a89cdddb0
David V. Smith <david.v.smith@temple.edu>
2024-05-26 09:26:49 -0400

    initial commit

---
+++ code/run_tedana.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+touch $scriptdir/missing-tedanaInput.log
+
+for sub in `cat ${scriptdir}/sublist-rf1.txt`; do
+	for run in 1 2; do
+		script=${scriptdir}/tedana.sh
+		NCORES=8
+		while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
+			sleep 5s
+		done
+		bash $script $sub $run &
+		sleep 5s
+	done
+done
---
+++ code/tedana.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
+
+sub=$1
+task=sharedreward
+run=$2
+
+# prepare inputs and outputs; don't run if data is missing, but log missingness
+prepdir=${maindir}/derivatives/fmriprep/sub-${sub}/func
+echo1=${maindir}/sub-${sub}_task-${task}_run-${run}_echo-1_part-mag_desc-preproc_bold.nii.gz
+echo2=${maindir}/sub-${sub}_task-${task}_run-${run}_echo-2_part-mag_desc-preproc_bold.nii.gz
+echo3=${maindir}/sub-${sub}_task-${task}_run-${run}_echo-3_part-mag_desc-preproc_bold.nii.gz
+echo4=${maindir}/sub-${sub}_task-${task}_run-${run}_echo-4_part-mag_desc-preproc_bold.nii.gz
+outdir=${maindir}/derivatives/tedana/sub-${sub}
+if [ ! -e $echo1 ]; then
+	echo "missing ${echo1}"
+	echo "missing ${echo1}" >> $scriptdir/missing-tedanaInput.log
+	exit
+fi
+mkdir -p $outdir
+
+# run tedana
+tedana -d $echo1 $echo2 $echo3 $echo4 \
+-e 0.0138 0.03154 0.04928 0.06702 \
+--out-dir $outdir \
+--prefix sub-${sub}_task-${task}_run-${run} \
+--convention bids \
+--fittype curvefit \
+--overwrite
+
+# clean up and save space
+rm -rf ${outdir}/*.nii.gz
