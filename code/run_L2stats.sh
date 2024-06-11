076c46624ae81c9c87619e073cc9b7ef968e53da
cooper-sharp <tun31934@temple.edu>
2024-06-10 20:36:41 -0400

    Add L2 scripts from trust directory -- need to adapt for SR

---
+++ code/run_L2stats.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
+
+# the "type" variable below is setting a path inside the main script
+for type in "act" ; do #"ppi_seed-VS_thr5" "act" "nppi-dmn"; do # act nppi-ecn #"ppi_seed-NAcc"
+	for sub in `cat ${scriptdir}/sublist_all.txt`; do
+	#for sub in 10529 10541 10572 10581 10584; do
+
+		# Manages the number of jobs and cores
+  	SCRIPTNAME=${maindir}/code/L2stats.sh
+  	NCORES=20
+  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+    		sleep 1s
+  	done
+  	bash $SCRIPTNAME $sub $type &
+  	sleep 1s
+
+	done
+done
